34ef9466d027b6d1f38be5505487be141813ba33
Mohit Alam Bhuiyan <mohitcseedu@gmail.com>
2023-05-28 23:26:13 +0600

    added on auth state change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
+	const {user, handleSignOut} = useFirebase();
 	return (
 		<div className="header">
-			<nav>
+			<nav> 
 				<Link to="/">Home</Link>
-				<Link to="/login">Login</Link>
+				<Link to="/products">Products</Link>
+				<Link to="/orders">Orders</Link>
+				<Link to="/register">Register</Link>
+				<span>{user?.displayName && user.displayName}</span>
+				{
+                    user?.uid 
+					?
+					<button onClick={handleSignOut}>sign out</button>
+					:
+					<Link to="/login">Login</Link>
+				}
 			</nav>
 		</div>
 	);
